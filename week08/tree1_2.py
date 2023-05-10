f7c7b927206a39701ae84f5892cf009c9a8be6ad
zinnnn37 <zinnnn37@gmail.com>
2023-05-10 13:24:43 +0900

    [Q1] 8: Runtime error(still\?!) && 9, 10: Time Out

--- week08/tree1_2.py
+++ week08/tree1_2.py
@@ -19,3 +19,66 @@ class Tree:
 			if res:
 				return res
 		return None
+
+	def add(self, val, parent=None):
+		node = Node(val)
+		if parent == None:
+			self.root = node
+			self.size += 1
+		else:
+			parent_node = self.search_node(self.root, parent)
+			parent_node.child.append(node)
+			self.size += 1
+
+
+def _preorder(pre, node):
+	if node == None:
+		return
+	pre.append(node.val)
+	for child in node.child:
+		_preorder(pre, child)
+	return pre
+
+def preorder(root):
+	pre = []
+	return _preorder(pre, root)
+
+def _postorder(post, node):
+	if node == None:
+		return
+	for child in node.child:
+		_postorder(post, child)
+	post.append(node.val)
+	return post
+
+def postorder(root):
+	post = []
+	return _postorder(post, root)
+
+def query_ancestor(a, b):
+	if pre.index(a) > pre.index(b):
+		return False
+	if post.index(a) < post.index(b):
+		return False
+	return True
+
+if __name__ == '__main__':
+	tree = Tree()
+	tree.add(1)
+
+	n, q = map(int, input().split())
+	for _ in range(n-1):
+		p, c = map(int, input().split())
+		tree.add(c, p)
+
+	pre = preorder(tree.root)
+	post = postorder(tree.root)
+
+	cnt = 0
+	for _ in range(q):
+		a, b = map(int, input().split())
+		if (query_ancestor(a, b)):
+			cnt += 1
+	print(cnt)
+
+# 엥 이것도 런타임 에러.. 심지어 9, 10번 타임아웃
