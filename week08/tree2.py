71906cc52bd5fac634423f72b919c4ce8dc6b453
zinnnn37 <zinnnn37@gmail.com>
2023-05-12 16:11:50 +0900

    [tree2] O(n)..

--- week08/tree2.py
+++ week08/tree2.py
@@ -10,6 +10,7 @@ def _postorder(q, visited, parent):
 			q.append(child)
 			_postorder(q, visited, cur)
 		if not visited[cur]:
+			parents[cur] = parent
 			w[parent] += w[cur]
 			visited[cur] = True
 
@@ -17,15 +18,14 @@ def postorder():
 	visited = [False] * (n + 1)
 	q = deque()
 	q.append(1)
-	_postorder(q, visited, 1)
-	w[1] //= 2
+	_postorder(q, visited, 0)
 
 def _update(q, diff):
 	while q:
 		cur = q.popleft()
-		for parent in rev[cur]:
-			q.append(parent)
-			w[parent] += diff
+		if parents[cur] != 0:
+			q.append(parents[cur])
+			w[parents[cur]] += diff
 			_update(q, diff)
 
 def update(v, diff):
@@ -37,14 +37,13 @@ def update(v, diff):
 if __name__ == '__main__':
 	n, q = map(int, input().split())
 	tree = [[] for _ in range(n + 1)]
-	rev = [[] for _ in range(n + 1)]
 	w = [0] + list(map(int, input().split()))
 
 	for _ in range(n-1):
 		p, c = map(int, input().split())
 		tree[p].append(c)
-		rev[c].append(p)
 
+	parents = [0] * (n + 1)
 	postorder()
 
 	for _ in range(q):
