97b94ad44965a4cd9c385f4a130235c11d98aaeb
zinnnn37 <zinnnn37@gmail.com>
2023-05-12 15:39:48 +0900

    [tree2] 뭘 고쳤지..

--- week08/tree2.py
+++ week08/tree2.py
@@ -1,58 +1,52 @@
 from collections import deque
-import copy
 import sys
 input = lambda: sys.stdin.readline().rstrip()
-sys.setrecursionlimit(10**8)
 
-def _postorder(q, visited, parent):
+def update()
+
+def _preorder(q, visited, parent):
 	while q:
 		cur = q.popleft()
+		if not visited[cur]:
+			pre.append(cur)
+			visited[cur] = True
+			cost[cur] += w[cur]
 		for child in tree[cur]:
 			q.append(child)
-			_postorder(q, visited, cur)
-		if not visited[cur]:
-			w[parent] += w[cur]
-			visited[cur] = True
+			subtree[cur] += 1
+			_preorder(q, visited, cur)
+		subtree[parent] += subtree[cur]
+		cost[parent] += cost[cur]
 
-def postorder():
+def preorder():
 	visited = [False] * (n + 1)
 	q = deque()
 	q.append(1)
-	_postorder(q, visited, 1)
-	w[1] //= 2
-
-def _update(q, diff):
-	while q:
-		cur = q.popleft()
-		for parent in rev[cur]:
-			q.append(parent)
-			w[parent] += diff
-			_update(q, diff)
-
-def update(v, diff):
-	q = deque()
-	q.append(v)
-	w[v] += diff
-	_update(q, diff)
+	_preorder(q, visited, 1)
+	subtree[1] //= 2
+	cost[1] //= 2
 
 if __name__ == '__main__':
 	n, q = map(int, input().split())
 	tree = [[] for _ in range(n + 1)]
-	rev = [[] for _ in range(n + 1)]
 	w = [0] + list(map(int, input().split()))
-	weight = copy.deepcopy(w)
 
 	for _ in range(n-1):
 		p, c = map(int, input().split())
 		tree[p].append(c)
-		rev[c].append(p)
 
-	postorder()
+	pre = []
+	cost = [0] * (n+1)
+	subtree = [0] * (n+1)
+	preorder()
+	print(pre)
+	print(subtree)
+	print(cost)
 
 	for _ in range(q):
 		cmd, *v = input().split()
 		v = list(map(int, v))
 		if cmd == 'subtree':
-			print(w[v[0]])
-		if cmd == 'update':
-			update(v[0], v[1])
+			print(cost[v[0]])
+		#if cmd == 'update':
+		#	update(v[0], v[1])
