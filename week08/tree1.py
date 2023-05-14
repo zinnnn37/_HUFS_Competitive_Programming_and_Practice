3e49546d3bb90a0b3adbfd0d20e22a580a02e67e
zinnnn37 <zinnnn37@gmail.com>
2023-05-14 18:58:50 +0900

    [week08] tree1: merge pre && post

--- week08/tree1.py
+++ week08/tree1.py
@@ -3,37 +3,20 @@ import sys
 sys.setrecursionlimit(10**8)
 input = lambda: sys.stdin.readline().rstrip()
 
-def _preorder(q, visited):
+def _dfs(q, visited):
 	while q:
 		cur = q.popleft()
-		if not visited[cur]:
-			pre.append(cur)
-			visited[cur] = True
+		pre.append(cur)
 		for child in tree[cur]:
 			q.append(child)
-			_preorder(q, visited)
+			_dfs(q, visited)
+		post.append(cur)
 
-def preorder():
+def dfs():
 	visited = [False] * (n + 1)
 	q = deque()
 	q.append(1)
-	_preorder(q, visited)
-
-def _postorder(q, visited):
-	while q:
-		cur = q.popleft()
-		for child in tree[cur]:
-			q.append(child)
-			_postorder(q, visited)
-		if not visited[cur]:
-			post.append(cur)
-			visited[cur] = True
-
-def postorder():
-	visited = [False] * (n + 1)
-	q = deque()
-	q.append(1)
-	_postorder(q, visited)
+	_dfs(q, visited)
 
 def query_ancestor(a, b):
 	if pre.index(a) > pre.index(b):
@@ -45,15 +28,13 @@ def query_ancestor(a, b):
 if __name__ == "__main__":
 	n, q = map(int, input().split())
 	tree = [[] for _ in range(n + 1)]
-	# 샹.. 결국에는 트리를 만들어야 한다
 
 	for _ in range(n-1):
 		a, b = map(int, input().split())
 		tree[a].append(b)
 
 	pre, post = [], []
-	preorder()
-	postorder()
+	dfs()
 
 	cnt = 0
 	for _ in range(q):
